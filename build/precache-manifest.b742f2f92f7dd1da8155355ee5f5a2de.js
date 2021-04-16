4911cce64081a0b6ac71995d7618ca8ac4602711
prahladinala <prahlad2018@gmail.com>
2021-04-16 15:00:15 +0530

    Initial Commit with Build Directory

---
+++ build/precache-manifest.b742f2f92f7dd1da8155355ee5f5a2de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc59154669e2ef6cea2409f815ed90a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cfcc35c4739a26b31b36",
+    "url": "./static/css/main.5921d42c.chunk.css"
+  },
+  {
+    "revision": "d1dfa66c2b9457d41de9",
+    "url": "./static/js/2.7089398b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7089398b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfcc35c4739a26b31b36",
+    "url": "./static/js/main.6c56b739.chunk.js"
+  },
+  {
+    "revision": "67d56f5fb0242dda4c0e",
+    "url": "./static/js/runtime-main.12b9418f.js"
+  }
+]);
